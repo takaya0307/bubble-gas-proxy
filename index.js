e5421f3dccb536641ec51d5ed79334b3a3db86d6
takaya0307 <takaya.0307@icloud.com>
2025-04-21 16:58:39 +0900

    fix: correct GAS endpoint

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.post("/proxy", async (req, res) => {
   try {
-    const gasUrl = "https://script.google.com/macros/s/AKfycbwjK7TbD1x67_B-ESZQNiCQYD0He1C-m1v1LqzM08xJIv7lrYO1K8xTeQfgztIzuDMr/exec";
+    const gasUrl = "https://script.google.com/macros/s/AKfycbzhDNMOS2Kv-0loSstahsrJfAkw8giysEbdxU8P1N0n8i9GrGDZTaoAi9EhVpR8louT/exec";
 
     // ステップ1: POSTを送信（リダイレクトを自動追わず、自分で処理）
     const postResponse = await axios.post(gasUrl, req.body, {
